5ce0655f400f31468a57a8aac3627d7dcc5f8f45
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-03 17:20:59 -0400

    Finish Assignment-3 Part 1 and Part 2

---
+++ assignments/assignment-3/solution.js
@@ -0,0 +1,28 @@
+// Part 1
+var num1 = 10;
+var num2 = 20;
+console.log(num1);
+console.log(num2);
+
+var addition = num1 + num2;
+console.log(addition);
+var substraction = num1 - num2;
+console.log(substraction);
+var multiply = num1 * num2;
+console.log(multiply);
+var division = num1 / num2;
+console.log(division);
+
+// Part 2
+
+var fahrenheit1 = 350;
+var celsius1 = (fahrenheit1 - 32) * (5 / 9);
+
+console.log(fahrenheit1);
+console.log(celsius1);
+
+var celsius2 = 180;
+var fahrenheit2 = celsius2 * (9 / 5) + 32;
+
+console.log(celsius2);
+console.log(fahrenheit2);
