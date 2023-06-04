0d7d7afd296fb232f610a1a4d49902eb260d96c2
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-03 21:00:01 -0400

    Finish Assignment-3 Part 3

--- assignments/assignment-3/solution.js
+++ assignments/assignment-3/solution.js
@@ -1,4 +1,4 @@
-// Part 1
+// Part 1: Basic Arithmetic Operations
 var num1 = 10;
 var num2 = 20;
 console.log(num1);
@@ -13,7 +13,7 @@ console.log(multiply);
 var division = num1 / num2;
 console.log(division);
 
-// Part 2
+// Part 2: Temperature Converter
 
 var fahrenheit1 = 350;
 var celsius1 = (fahrenheit1 - 32) * (5 / 9);
@@ -26,3 +26,11 @@ var fahrenheit2 = celsius2 * (9 / 5) + 32;
 
 console.log(celsius2);
 console.log(fahrenheit2);
+
+// Part 3: Interest Calculator
+
+var principal = 10000;
+var rate = 3.5;
+var time = 5;
+var interest = (principal * rate * time) / 100;
+console.log(interest);
