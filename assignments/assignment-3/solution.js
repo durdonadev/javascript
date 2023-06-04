98a98f0892916a3a339e3c735706174bc03e8032
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-03 21:20:45 -0400

    Finish Assignment-3 Part 4

--- assignments/assignment-3/solution.js
+++ assignments/assignment-3/solution.js
@@ -1,31 +1,31 @@
 // Part 1: Basic Arithmetic Operations
 var num1 = 10;
 var num2 = 20;
-console.log(num1);
-console.log(num2);
+console.log("num1 =", num1);
+console.log("num2 =", num2);
 
 var addition = num1 + num2;
-console.log(addition);
+console.log("num1 + num2 =", addition);
 var substraction = num1 - num2;
-console.log(substraction);
+console.log("num1 - num2 =", substraction);
 var multiply = num1 * num2;
-console.log(multiply);
+console.log("num1 * num2 =", multiply);
 var division = num1 / num2;
-console.log(division);
+console.log("num1 / num2 =", division);
 
 // Part 2: Temperature Converter
 
 var fahrenheit1 = 350;
 var celsius1 = (fahrenheit1 - 32) * (5 / 9);
 
-console.log(fahrenheit1);
-console.log(celsius1);
+console.log("F =", fahrenheit1);
+console.log("C =", celsius1);
 
 var celsius2 = 180;
 var fahrenheit2 = celsius2 * (9 / 5) + 32;
 
-console.log(celsius2);
-console.log(fahrenheit2);
+console.log("C =", celsius2);
+console.log("F =", fahrenheit2);
 
 // Part 3: Interest Calculator
 
@@ -33,4 +33,15 @@ var principal = 10000;
 var rate = 3.5;
 var time = 5;
 var interest = (principal * rate * time) / 100;
-console.log(interest);
+console.log("Interest =", interest);
+
+// Part 4: Swapping Values
+
+var a = 35;
+var b = 15;
+
+a = a + b;
+b = a - b;
+
+console.log("a =", a);
+console.log("b =", b);
