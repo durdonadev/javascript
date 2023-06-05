cf2c13aad12e8e8fb8d10a5e4fbc42f9a3e7bceb
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-05 11:50:31 -0400

    Finish Assignment-5

---
+++ assignments/assignment-5/solution.js
@@ -0,0 +1,15 @@
+var name = "Durdona";
+var age = 27;
+var cityOfResidence = "New York";
+var profession = "a software engineer";
+var hobby1 = "listening to audio books";
+var hobby2 = "playing with my baby";
+var favFood = "plov";
+var favColor = "light violet";
+var dreamTravelDestination = "Makkah";
+var favanimal = "horse";
+var favSeason = "Spring";
+
+var result = `Hello, my name is ${name} and I am ${age} years old. I live in ${cityOfResidence}, where I work as ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favFood} and my favorite color is ${favColor}. My dream is one day to visit ${dreamTravelDestination}. My favorite season of the year is ${favSeason} because of the beautiful changing colors of the leaves.`;
+
+console.log(result);
