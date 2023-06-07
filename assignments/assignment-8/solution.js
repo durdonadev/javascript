ff8f6d58091258e6552327b6cb9aef9ecf77a1ee
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-07 11:37:12 -0400

    Finish Assignment-8 | Random numbers

---
+++ assignments/assignment-8/solution.js
@@ -0,0 +1,50 @@
+// a. Between 1 and 10
+console.log(
+    "Random number between 1 and 10:",
+    Math.floor(Math.random() * 10) + 1
+);
+// b. Between 1 and 100
+console.log(
+    "Random number between 1 and 100:",
+    Math.floor(Math.random() * 100) + 1
+);
+// c. Between 50 and 100
+console.log(
+    "Random number between 50 and 100:",
+    Math.floor(Math.random() * (100 - 50 + 1)) + 50
+);
+// d. Between -10 and 10
+console.log(
+    "Random number between -10 and 10:",
+    Math.floor(Math.random() * (10 - -10 + 1)) - 10
+);
+// e. Between -100 and 100
+console.log(
+    "Random number between -100 and 100:",
+    Math.floor(Math.random() * (100 - -100 + 1)) - 100
+);
+// f. Between 1000 and 2000
+console.log(
+    "Random number between 1000 and 2000:",
+    Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000
+);
+// g. Between -1000 and 0
+console.log(
+    "Random number between -1000 and 0:",
+    Math.floor(Math.random() * (0 - -1000 + 1)) - 1000
+);
+// h. Between 1 and 1000
+console.log(
+    "Random number between 1 and 1000:",
+    Math.floor(Math.random() * (1000 - 1 + 1)) + 1
+);
+// i. Between -500 and 500
+console.log(
+    "Random number between -500 and 500:",
+    Math.floor(Math.random() * (500 - -500 + 1)) - 500
+);
+// j. Between 1000 and 2000
+console.log(
+    "Random number between 1000 and 2000:",
+    Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000
+);
