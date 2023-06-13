f21afeddb0d47e2581bf161e2be31977d5d942a8
Durdona Ibragimova <durdona.dev@gmail.com>
2023-06-12 23:09:27 -0400

    Correct codes in Assignment-13

--- assignments/assignment-13/solution.js
+++ assignments/assignment-13/solution.js
@@ -1,66 +1,57 @@
 // 1
 function combineArrays(arr1, arr2) {
-    var result = arr1.concat(arr2);
-    return result;
+    return arr1.concat(arr2);
 }
 console.log(combineArrays([1, 2, 3], [4, 5, 6]));
 
 // 2
 function reverseOrder(arr) {
-    var result = arr.reverse();
-    return result;
+    return arr.reverse();
 }
 console.log(reverseOrder([1, 2, 3]));
 
 // 3
 function checkValue(arr, num) {
-    var result = arr.includes(num);
-    return result;
+    return arr.includes(num);
 }
 console.log(checkValue([1, 2, 3], 2));
 
 // 4
 function isArray(arr) {
-    var result = Array.isArray(arr);
-    return result;
+    return Array.isArray(arr);
 }
 console.log(isArray([1, 2, 3]));
 
 // 5
 function arraySlice(arr, num1, num2) {
-    var result = arr.slice(num1, num2);
-    return result;
+    return arr.slice(num1, num2);
 }
 console.log(arraySlice([1, 2, 3, 4, 5], 1, 4));
 
 // 6
 function removeLast(arr) {
-    var lastElement = arr.pop();
-    var result = arr;
-    return result;
+    arr.pop();
+    return arr;
 }
 console.log(removeLast([1, 2, 3]));
 
 // 7
 function removeFirst(arr) {
-    var lastElement = arr.shift();
-    var result = arr;
-    return result;
+    arr.shift();
+    return arr;
 }
 console.log(removeFirst([1, 2, 3]));
 
 // 8
 function addAtBeginning(arr, num) {
-    var newLength = arr.unshift(num);
-    var result = arr;
-    return result;
+    arr.unshift(num);
+    return arr;
 }
 console.log(addAtBeginning([1, 2, 3], 0));
 
 // 9
 function addAtEnd(arr, num) {
-    var newLength = arr.push(num);
-    var result = arr;
-    return result;
+    arr.push(num);
+    return arr;
 }
 console.log(addAtEnd([1, 2, 3], 4));
